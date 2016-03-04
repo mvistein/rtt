732dcea1931367e57168825d1d3f101d25b6b035
Michael Vistein <michael.vistein@dlr.de>
2016-03-04 13:12:31 +0100

    detect Xenomai >= 3 and disable IRQActivity

--- rtt/extras/IRQActivity.hpp
+++ rtt/extras/IRQActivity.hpp
@@ -38,88 +38,95 @@
 
 #ifndef IRQ_ACTIVITY_HPP
 #define IRQ_ACTIVITY_HPP
-//
-//#include "../Activity.hpp"
-//#include "../rtt-config.h"
-//
-//#ifdef OROPKG_OS_XENOMAI
-//#include <native/intr.h>
-//#else
-//// Provide a dump RT_INTR type. start() always returns false on non-Xenomai
-//// OSes.
-//struct RT_INTR { };
-//#ifndef RTT_DLL_EXPORT
-//#ifdef _MSC_VER
-//	#pragma message( "IRQActivity is available only on Xenomai. It will fail on startup")
-//#else
-//#warning "IRQActivity is available only on Xenomai. It will fail on startup"
-//#endif
-//#endif
-//#endif
-//
-//namespace RTT { namespace extras {
-//
-//    /** An activity which is triggered by an IRQ edge. This activity is only
-//     * available on xenomai
-//     *
-//     * The IRQ line can be given by two means:
-//     * <ul>
-//     *   <li> setIRQ is called
-//     *   <li> one of the underlying task contexts (if any) provides the
-//     *   Provider interface. In that case, the getIRQ() method of
-//     *   that task context is called to get the descriptor
-//     * </ul>
-//     */
-//    class RTT_API IRQActivity : public Activity
-//    {
-//        int      m_irq;
-//        RT_INTR  m_handle;
-//        base::RunnableInterface* m_runner;
-//
-//    public:
-//        /** If this interface is derived from one of the TaskContext objects
-//         * which are executed by this activity, then the activity can
-//         * auto-configure itself (i.e. it can get the file descriptor from
-//         * that task context). Otherwise, setIRQ must be called
-//         * explicitely
-//         */
-//        struct Provider {
-//            virtual int getIRQ() const = 0;
-//        };
-//
-//        /**
-//         * Create a IRQActivity with a given priority and base::RunnableInterface
-//         * instance. The default scheduler for IRQActivity
-//         * objects is ORO_SCHED_RT.
-//         *
-//         * @param priority The priority of the underlying thread.
-//         * @param _r The optional runner, if none, this->loop() is called.
-//         */
-//        IRQActivity(int priority, base::RunnableInterface* _r = 0, const std::string& name ="IRQActivity" );
-//
-//        /**
-//         * Create a IRQActivity with a given scheduler type, priority and
-//         * base::RunnableInterface instance.
-//         * @param scheduler
-//         *        The scheduler in which the activitie's thread must run. Use ORO_SCHED_OTHER or
-//         *        ORO_SCHED_RT.
-//         * @param priority The priority of the underlying thread.
-//         * @param _r The optional runner, if none, this->loop() is called.
-//         */
-//        IRQActivity(int scheduler, int priority, base::RunnableInterface* _r = 0, const std::string& name ="IRQActivity" );
-//
-//        virtual ~IRQActivity();
-//
-//        int  getIRQ() const;
-//        void setIRQ(int irq);
-//
-//        virtual bool start();
-//#ifdef OROPKG_OS_XENOMAI
-//        virtual void loop();
-//        virtual bool breakLoop();
-//        virtual void step();
-//#endif
-//    };
-//}}
+
+#include "../Activity.hpp"
+#include "../rtt-config.h"
+
+#ifdef OROPKG_OS_XENOMAI
+#include <xeno_config.h>
+#if ((CONFIG_XENO_VERSION_MAJOR*1000)+(CONFIG_XENO_VERSION_MINOR*100)+CONFIG_XENO_REVISION_LEVEL) < 3000
+#define XENOMAI2
+#endif
+#endif
+
+#ifdef XENOMAI2
+#include <native/intr.h>
+#else
+// Provide a dump RT_INTR type. start() always returns false on non-Xenomai
+// OSes.
+struct RT_INTR { };
+#ifndef RTT_DLL_EXPORT
+#ifdef _MSC_VER
+	#pragma message( "IRQActivity is available only on Xenomai. It will fail on startup")
+#else
+#warning "IRQActivity is available only on Xenomai. It will fail on startup"
+#endif
+#endif
+#endif
+
+namespace RTT { namespace extras {
+
+    /** An activity which is triggered by an IRQ edge. This activity is only
+     * available on xenomai
+     *
+     * The IRQ line can be given by two means:
+     * <ul>
+     *   <li> setIRQ is called
+     *   <li> one of the underlying task contexts (if any) provides the
+     *   Provider interface. In that case, the getIRQ() method of
+     *   that task context is called to get the descriptor
+     * </ul>
+     */
+    class RTT_API IRQActivity : public Activity
+    {
+        int      m_irq;
+        RT_INTR  m_handle;
+        base::RunnableInterface* m_runner;
+
+    public:
+        /** If this interface is derived from one of the TaskContext objects
+         * which are executed by this activity, then the activity can
+         * auto-configure itself (i.e. it can get the file descriptor from
+         * that task context). Otherwise, setIRQ must be called
+         * explicitely
+         */
+        struct Provider {
+            virtual int getIRQ() const = 0;
+        };
+
+        /**
+         * Create a IRQActivity with a given priority and base::RunnableInterface
+         * instance. The default scheduler for IRQActivity
+         * objects is ORO_SCHED_RT.
+         *
+         * @param priority The priority of the underlying thread.
+         * @param _r The optional runner, if none, this->loop() is called.
+         */
+        IRQActivity(int priority, base::RunnableInterface* _r = 0, const std::string& name ="IRQActivity" );
+
+        /**
+         * Create a IRQActivity with a given scheduler type, priority and
+         * base::RunnableInterface instance.
+         * @param scheduler
+         *        The scheduler in which the activitie's thread must run. Use ORO_SCHED_OTHER or
+         *        ORO_SCHED_RT.
+         * @param priority The priority of the underlying thread.
+         * @param _r The optional runner, if none, this->loop() is called.
+         */
+        IRQActivity(int scheduler, int priority, base::RunnableInterface* _r = 0, const std::string& name ="IRQActivity" );
+
+        virtual ~IRQActivity();
+
+        int  getIRQ() const;
+        void setIRQ(int irq);
+
+        virtual bool start();
+#ifdef XENOMAI2
+        virtual void loop();
+        virtual bool breakLoop();
+        virtual void step();
+#endif
+    };
+}}
 
 #endif
